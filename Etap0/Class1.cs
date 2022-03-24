b3ee9f9b3a7a526590e175dc9b00af21d913ca55
236657 Wojciech Stefaniak <236657@edu.p.lodz.pl>
2022-03-24 21:59:57 +0100

    Program-Etap0

---
+++ Etap0/Class1.cs
@@ -0,0 +1,25 @@
+﻿namespace Etap0
+{
+    public class Calculator
+    {
+        public double Add(double a, double b)
+        {
+            return a+b;
+        }
+
+        public double Sub(double a, double b)
+        {
+            return a - b;
+        }
+
+        public double Mul(double a, double b)
+        {
+            return a * b;
+        }
+
+        public double Div(double a, double b)
+        {
+            return a / b;
+        }
+    }
+}
